2d8f62de085788472b177bbd7b5cce27f4cc5277
gamusiro <gamusiro310@gmail.com>
2024-09-14 08:47:53 +0900

    source commit

---
+++ app/src/main/java/com/example/onlineshop/Activity/IntroActivity.kt
@@ -0,0 +1,24 @@
+package com.example.onlineshop.Activity
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.onlineshop.R
+import com.example.onlineshop.databinding.ActivityIntroBinding
+
+class IntroActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityIntroBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityIntroBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.startBtn.setOnClickListener{
+            startActivity(Intent(this@IntroActivity, MainActivity::class.java))
+        }
+    }
+}
